183167047e937a5f3c770fdbc463931a70b88acb
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2021-09-15 13:53:07 +0900

    update

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,36 +1,46 @@
-import React from 'react'
+import React,{ useState } from 'react';
 
 import { AppBar, Toolbar, IconButton } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 
-// import SideDrawer from './SideDrawer'
+import SideDrawer from './SideDrawer'
 
-const Header = () => {
-  return (
-    // <div>あああ</div>
-    <AppBar
-      position="fixed"
-      style={{
-        backgroundColor: '#2f2f2f',
-        boxShadow: 'none',
-        padding: '10px 0px'
-      }}
-    >
-     <Toolbar>
-      <div className="header_logo">
-        <div className="font_righteous header_logo_venue">The venue</div>
-        <div className="header_logo_title">Musical Events</div>
-      </div>
-      <IconButton
-        aria-label="Menu"
-        color="inherit"
+const Header = ()=> {
+  const [drawerOpen,setDrawerOpen] = useState(false)
+
+
+  const toggleDrawer = (value) => {
+      setDrawerOpen(value)
+  }
+
+
+  return(
+      <AppBar
+        position="fixed"
+        style={{
+            backgroundColor:'#2f2f2f',
+            boxShadow:'none',
+            padding:'10px 0px'
+        }}
       >
-        <MenuIcon />
-      </IconButton>
-      {/* <SideDrawer /> */}
-     </Toolbar>
-    </AppBar>
-  )
+        <Toolbar>
+          <div className="header_logo">
+            <div className="font_righteous header_logo_venue">The venue</div>
+            <div className="header_logo_title">Musical Events</div>
+          </div>
+          <IconButton
+            aria-label="Menu"
+            color="inherit"
+            onClick={()=> toggleDrawer(true)}
+          >
+            <MenuIcon/>
+          </IconButton>
+          <SideDrawer
+            open={drawerOpen}
+            onClose={(value)=>toggleDrawer(value) }
+          />
+        </Toolbar>
+      </AppBar>
+    )
 }
-
-export default Header
+export default Header;
