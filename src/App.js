60d56518d1640678ce318a42d8743898f3aa2eef
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2021-09-15 19:55:39 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './resources/styles.css'
 import Header from './components/header_footer/Header'
+import Footer from './components/header_footer/Footer'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <div style={{backgroundColor:'blue',height:'800px'}}></div>
       <div style={{backgroundColor:'green',height:'800px'}}></div>
       <div style={{backgroundColor:'yellow',height:'800px'}}></div>
+      <Footer />
     </div>
   )
 }
