8cde8caa45871863915bb0cff36ee88bb9c2555a
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2021-09-21 16:02:53 +0900

    update

--- src/components/utils/MyButton.js
+++ src/components/utils/MyButton.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import Button from '@material-ui/core/Button'
+import React from 'react';
+import Button from '@material-ui/core/Button';
 
-import TicketIcon from '../../resources/images/icons/ticket.png'
+import TicketIcon from '../../resources/images/icons/ticket.png';
 
 const MyButton = (props) => {
-  return (
-    <Button>
-      { props.iconTiket ? <img src={TicketIcon} className="iconImage" alt="icon_button" /> : null }
+  return(
+    <Button
+      href={props.link}
+      variant="contained"
+      size={ props.size ? props.size:'large'}
+      style={{
+        background:'#8e8e8e',
+        color:'#ffffff',
+        ...props.style
+      }}
+    >    
+      { props.iconTicket ?
+        <img
+          src={TicketIcon}
+          className="iconImage"
+          alt="icon_button"
+        />
+      :null}
       {props.text}
     </Button>
   )
